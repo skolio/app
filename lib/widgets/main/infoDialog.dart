ab7008ed47cd1458d03feb559023787fe34a7356
Brahim Zeqiraj <brahim.zeqiraj@hotmail.com>
2021-11-12 13:16:38 +0100

    infodialog finished

--- lib/widgets/main/infoDialog.dart
+++ lib/widgets/main/infoDialog.dart
@@ -9,20 +9,43 @@ class _InfoDialogState extends State<InfoDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
       child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(
+            20,
+          ),
+        ),
+        padding: EdgeInsets.all(8),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            SizedBox(height: 10),
             Container(
               child: Text(
                 "Skolio ist eine Anwendung zur Unterstützung Deiner Skoliose-Therapie. Das Training mit Skolio sollte nur nach Anleitung durch einen geschulten Therapeuten erfolgen.",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withOpacity(0.8),
+                ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("OK"),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 30,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Bestätigen"),
+              ),
             ),
+            SizedBox(height: 5),
           ],
         ),
       ),
